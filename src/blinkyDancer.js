14a936487ef20306a3946dccba9add8946746d34
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-22 16:57:00 -0800

    initial progress

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,24 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+  Dancer.call(this, top, left, timeBetweenSteps);
+  
+  
 
-  return blinkyDancer;
 };
+
+BlinkyDancer.prototype = new Dancer(this.top, this.left, this.timeBetweenSteps);
+
+BlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  
+  
+  this.$node.toggle();
+};
+
+
+
